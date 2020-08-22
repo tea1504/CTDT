a8fff0a19d77f3669b2420f272bbb71d676d03b7
Văn Hòa Trần <tranvanhoa15042000@gmail.com>
2020-08-22 10:58:18 +0700

    update index.php anf style.css

--- index.php
+++ index.php
@@ -8,11 +8,37 @@
 </head>
 <body>
     <div id="main">
-        <div id="chinh" class="mon">
-            <span class="mon_ma">CT103</span>
-            <p class="mon_ten">Cấu Trúc Dữ Liệu</p>
-            <div class="icon" id="icon1">+</div>
-            <div class="icon" id="icon2">+</div>
+        <div id="chinh">
+            <div class="mon">
+                <span class="mon_ma">CT103</span>
+                <p class="mon_ten">Cấu Trúc Dữ Liệu</p>
+                <div class="icon tooltip" id="icon1">+</div>
+                <div class="icon tooltip" id="icon2">+</div>
+            </div>
+        </div>
+        <div id="tquyet">
+            <div class="an">
+                <div class="mon">
+                    <span class="mon_ma">CT101</span>
+                    <p class="mon_ten">Lập Trình Căn Bản A</p>
+                </div>
+            </div>
+        </div>
+        <div id="sau">
+            <div class="an">
+                <div class="mon">
+                    <span class="mon_ma">CT174</span>
+                    <p class="mon_ten">PT Và TK Thuật Toán</p>
+                </div>
+                <div class="mon">
+                    <span class="mon_ma">CT175</span>
+                    <p class="mon_ten">Lý Thuyết Đồ Thị</p>
+                </div>
+                <div class="mon">
+                    <span class="mon_ma">CT180</span>
+                    <p class="mon_ten">Cơ Sở Dữ Liệu</p>
+                </div>
+            </div>
         </div>
     </div>
     <script src="vender\jquery\jquery-3.5.1.min.js"></script>
@@ -20,17 +46,23 @@
         $(document).ready(function() {
             $('#icon1').on('click', function() {
                 var t = document.getElementById("icon1").innerText;
-                if(t=='+')
+                if(t=='+'){
                     $('#icon1').text("-");
-                else
+                }
+                else{
                     $('#icon1').text("+");
+                }
+                $('#tquyet>div').toggleClass('an');
             });
             $('#icon2').on('click', function() {
                 var t = document.getElementById("icon2").innerText;
-                if(t=='+')
+                if(t=='+'){
                     $('#icon2').text("-");
-                else
+                }
+                else{
                     $('#icon2').text("+");
+                }
+                $('#sau>div').toggleClass('an');
             });
         });
     </script>

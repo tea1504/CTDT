eac716a0b549eb606413bf30c0b94a914af8276e
Văn Hòa Trần <tranvanhoa15042000@gmail.com>
2020-08-24 21:17:34 +0700

    update

--- index.php
+++ index.php
@@ -42,6 +42,29 @@
         </div>
     </div>
     <script src="vender\jquery\jquery-3.5.1.min.js" type="text/javascript"></script>
-    <script scr="js\app.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#icon1').on('click', function() {
+                var t = document.getElementById("icon1").innerText;
+                if(t=='+'){
+                    $('#icon1').text("-");
+                }
+                else{
+                    $('#icon1').text("+");
+                }
+                $('#tquyet>div').toggleClass('an');
+            });
+            $('#icon2').on('click', function() {
+                var t = document.getElementById("icon2").innerText;
+                if(t=='+'){
+                    $('#icon2').text("-");
+                }
+                else{
+                    $('#icon2').text("+");
+                }
+                $('#sau>div').toggleClass('an');
+            });
+        });
+    </script>
 </body>
 </html>
